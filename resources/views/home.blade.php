fc597ce70d8166084f8c5901a8c1af3468837607
fraberret <f.berretta02@gmail.com>
2024-05-07 19:08:13 +0200

    add banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,19 +6,32 @@
 </div>
 
 <div class="products_section bg-dark">
-
-    <div class="container-sm py-5">
-        <div class="row">
+    <div class=" container-sm py-5 position-relative">
+        <span class="series position-absolute bg_primary px-3 py-2  text-white">CURRENT SERIES</span>
+        <div class="row gy-3">
             @foreach ($products as $product)
             <div class="col-2 ">
                 <div class="imgholder ">
                     <img class="card-img-top" src="{{$product['thumb']}}" alt="">
                 </div>
-                <h5 class="text-white">{{$product['title']}}</h5>
+                <p class="text-white text-uppercase">{{$product['title']}}</p>
 
             </div>
             @endforeach
         </div>
+        <button class="position-absolute bottom-20 start-50 translate-middle-x bg_primary text-white px-5 py-2 border-0">LOAD MORE</button>
+    </div>
+</div>
+
+<div class="bluebanner bg_primary">
+    <div class="container py-3">
+
+        <img src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="">
+        <img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="">
+        <img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="">
+        <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="">
+        <img src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="">
+
     </div>
 </div>
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,30 +1,50 @@
 <header>
-    <div class="banner">
+    <div class="banner bg_primary">
         <div class="container">
             <p class="text-end">Lorem ipsum .</p>
         </div>
     </div>
-    <div class="container py-3 d-flex justify-content-between align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
         <div class="logo">
             <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="">
         </div>
         <nav class="navbar navbar-expand-lg ">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" aria-current="page" href="#">CHARACTERS</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="#">COMICS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">MOVIES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">TV</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">GAMES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">COLLECTIBLES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">VIDEOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">FANS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">NEWS</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
+                                SHOP
                             </a>
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="#">Action</a></li>
@@ -35,9 +55,7 @@
                                 <li><a class="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
+
                     </ul>
 
                 </div>
