8273f2bc30ef991c87a1fd208f9c468f56d230a1
fraberret <f.berretta02@gmail.com>
2024-05-07 17:05:58 +0200

    import comics by config

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $products = config('db');
+    @dd($products);
+
+    return view('home', compact('products'));
 })->name('home');
