8f079435e8916c0a669b821e6b9a0b403e886a34
fraberret <f.berretta02@gmail.com>
2024-05-07 17:12:56 +0200

    import comics in page by foreach cycle

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,17 @@
 
 @section('content')
 <h1>HomePage</h1>
+
+<div class="container">
+    <div class="row">
+        @foreach ($products as $product)
+        <div class="col">
+            <div class="card">
+                <img class="card-img-top" src="{{$product['thumb']}}" alt="">
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     $products = config('db');
-    @dd($products);
+    /* @dd($products); */
 
     return view('home', compact('products'));
 })->name('home');
