42bb9f43f3d5234358c7f1e759572ee6def09068
fraberret <f.berretta02@gmail.com>
2024-05-08 16:47:16 +0200

    add socials section on footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,54 +1,70 @@
 <footer>
-    <div class="container_m d-flex justify-content-between">
-        <div class="container_xs">
-            <div class="row gx-5">
-                <div class="col py-4 text-white">
-                    <h4>DC Comics</h4>
-                    <ul class="p-0 text-start">
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
+    <div class="up_section">
+        <div class="container_m d-flex justify-content-between">
+            <div class="container_xs">
+                <div class="row gx-5">
+                    <div class="col py-4 text-white">
+                        <h4>DC Comics</h4>
+                        <ul class="p-0 text-start">
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
 
-                    </ul>
-                </div>
-                <div class="col py-4 text-white">
-                    <h4>DC Comics</h4>
-                    <ul class="p-0 text-start">
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
+                        </ul>
+                    </div>
+                    <div class="col py-4 text-white">
+                        <h4>DC Comics</h4>
+                        <ul class="p-0 text-start">
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
 
-                    </ul>
-                </div>
-                <div class="col py-4 text-white">
-                    <h4>DC Comics</h4>
-                    <ul class="p-0 text-start">
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
+                        </ul>
+                    </div>
+                    <div class="col py-4 text-white">
+                        <h4>DC Comics</h4>
+                        <ul class="p-0 text-start">
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
 
-                    </ul>
-                </div>
-                <div class="col py-4 text-white">
-                    <h4>DC Comics</h4>
-                    <ul class="p-0 text-start">
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
+                        </ul>
+                    </div>
+                    <div class="col py-4 text-white">
+                        <h4>DC Comics</h4>
+                        <ul class="p-0 text-start">
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
 
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
+
             </div>
-
+            <img class="ps-5" src="{{ Vite::asset('resources/img/dc-logo-BG.png') }}" alt="">
         </div>
-        <img class="ps-5" src="{{ Vite::asset('resources/img/dc-logo-BG.png') }}" alt="">
     </div>
+    <div class="sign-up bg-dark">
+        <div class="container py-4 d-flex justify-content-between align-items-center">
+            <button class="btn border-primary border text-white">SIGN-UP NOW!</button>
+            <div class="socials d-flex align-items-center">
+                <h5 class=" pe-4 p-0 m-0 cl_primary">FOLLOW US</h5>
+                <img class="pe-2" src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                <img class="pe-2" src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                <img class="pe-2" src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                <img class="pe-2" src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                <img class="pe-2" src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+            </div>
+        </div>
+    </div>
+
 </footer>
