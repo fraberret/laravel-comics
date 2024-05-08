4d1470724a230b289e759f69804fae440f03dae3
fraberret <f.berretta02@gmail.com>
2024-05-08 16:18:06 +0200

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,54 @@
 <footer>
-    This is the footer
+    <div class="container_m d-flex justify-content-between">
+        <div class="container_xs">
+            <div class="row gx-5">
+                <div class="col py-4 text-white">
+                    <h4>DC Comics</h4>
+                    <ul class="p-0 text-start">
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+
+                    </ul>
+                </div>
+                <div class="col py-4 text-white">
+                    <h4>DC Comics</h4>
+                    <ul class="p-0 text-start">
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+
+                    </ul>
+                </div>
+                <div class="col py-4 text-white">
+                    <h4>DC Comics</h4>
+                    <ul class="p-0 text-start">
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+
+                    </ul>
+                </div>
+                <div class="col py-4 text-white">
+                    <h4>DC Comics</h4>
+                    <ul class="p-0 text-start">
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+        <img class="ps-5" src="{{ Vite::asset('resources/img/dc-logo-BG.png') }}" alt="">
+    </div>
 </footer>
